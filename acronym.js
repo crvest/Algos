5bb7f60408cde58f5eab86713e9ed5503257ade1
Chad <chad.vest@gmail.com>
2023-03-20 10:31:08 -0400

    finished acronym

--- acronym.js
+++ acronym.js
@@ -5,7 +5,28 @@
 
 
 function acronym(str) {
-    
+newStr = str[0];
+
+for (i = 0; i < str.length; i++){
+    console.log(str[i]);
+    if(str[i] == " " && str[i+1] != " "){
+        newStr += str[i+1];
+    }
+}
+return newStr.toUpperCase()
+}
+
+console.log(acronym("there's no free lunch - gotta pay yer way"));
+
+
+// array solution
+function acronym(str) {
+    arr = str.split(" ");
+    newArr = [];
+    for(i = 0; i < arr.length; i++) {
+        newArr.push(arr[i][0].toUpperCase());
+    }
+    return newArr.join("")
 }
 
 console.log(acronym("there's no free lunch - gotta pay yer way"));
